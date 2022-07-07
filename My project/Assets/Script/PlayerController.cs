e346df6b314460818405f9407173d10d1966fa13
Đạt <77010056+GauTrangWibu@users.noreply.github.com>
2022-07-07 15:32:15 +0700

    Update Movement

--- My project/Assets/Script/PlayerController.cs
+++ My project/Assets/Script/PlayerController.cs
@@ -5,26 +5,42 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    [SerializeField] public float speed;
     [SerializeField] private Rigidbody2D rb;
-    void Awake()
+    [SerializeField] private float horizontalValue;
+    [SerializeField] private bool IsFaceRight = true ;
+    private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
+        horizontalValue = Input.GetAxisRaw("Horizontal");
         
     }
 
-    void FixedUdate()
+    private void FixedUpdate()
     {
+        Move(horizontalValue);
+    }
 
+
+    void Move(float direction)
+    {
+        float xValue = direction * speed* Time.deltaTime;
+        Vector2 targetVelocity = new Vector2(xValue, rb.velocity.y);
+        rb.velocity = targetVelocity;
+        if(IsFaceRight && direction < 0)
+        {
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+            IsFaceRight = false;
+        }
+        else if (!IsFaceRight && direction > 0)
+        {
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+            IsFaceRight = true;
+        }
+        
     }
 }
