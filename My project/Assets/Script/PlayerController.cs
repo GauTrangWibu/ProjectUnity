60fc530dcb24e029b0ba81be75841880bf837645
Đạt <77010056+GauTrangWibu@users.noreply.github.com>
2022-07-07 14:25:10 +0700

    Update empty project

---
+++ My project/Assets/Script/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] private Rigidbody2D rb;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUdate()
+    {
+
+    }
+}
