55fa0ebe206fb73936e9b4d811bf13fc0c031fdd
Đạt <77010056+GauTrangWibu@users.noreply.github.com>
2022-07-12 21:20:49 +0700

    Update EnemyAI.cs

--- My project/Assets/Script/EnemyAI.cs
+++ My project/Assets/Script/EnemyAI.cs
@@ -92,4 +92,12 @@ public class EnemyAI : MonoBehaviour
            
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("Trigger on");
+        }
+    }
 }
