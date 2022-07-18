34ff1b30e972258176cce541e5bd02a41a0e9d47
Đạt <77010056+GauTrangWibu@users.noreply.github.com>
2022-07-18 21:46:16 +0700

    Add function Add score

---
+++ My project/Assets/Script/AddScore.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class AddScore : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI scoreText;
+    private int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "AddScore")
+        {
+            score += 10;
+            scoreText.text = "Score:" + score;
+        }
+    }
+
+    
+}
